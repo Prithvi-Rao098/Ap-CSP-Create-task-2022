d36fdcd00892c638d70b8b3117116e36ed356a35
Prithvi Rao <73247272+Broccoli201@users.noreply.github.com>
2022-03-25 15:00:10 -0700

    ASD
    ASD

--- links.py
+++ links.py
@@ -1,3 +1,8 @@
-https://www.pinterest.com/pin/515169644852313684/       #  final boss?
+https: // www.pinterest.com/pin/515169644852313684/  # final boss?
 
-http://decrodedart.blogspot.com/    # final boss 
+http: // decrodedart.blogspot.com/    # final boss
+
+
+https: // www.pinterest.com/pin/160088961739053448/  # background
+
+HAVE TO LINK THE MAIN CHARACTER
