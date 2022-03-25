ec439c202442d98de34cead65c4dc0d3f0d32c19
Prithvi Rao <73247272+Broccoli201@users.noreply.github.com>
2022-03-25 14:54:17 -0700

    adding background

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-########################################
-#              Prithvi Rao             #
-#                                      #
-#         --- enter name ---       #
-#                                      #
-#   period: 6                          #
-#   Description:
-
 import pygame
+import os
 pygame.font.init()
 
 # initilizing the SCREENdow screen
-WIDTH, HEIGHT = 1000,900
+WIDTH, HEIGHT = 1000, 900
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 BACKGROUND = (234, 212, 252)
-pygame.display.set_caption("Space Invader - Prithvi Rao")                                                                                       # ADD NAME
 
-# 
+
+# ADD NAME
+pygame.display.set_caption(
+    " !!!@@@@ ----  ENTER NAME LATER  ----@@@@!!!  - Prithvi Rao")
+
+
+# initializing the characters and the images
 run = True
 
 while run:
     for event in pygame.event.get():
-        title_label = title_font.render("Press the mouse to begin", 1, (250,0,0))
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
