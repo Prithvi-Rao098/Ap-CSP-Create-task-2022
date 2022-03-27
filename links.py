2db42bcf521ddcffa058d76cb1cfbf1f63b80a53
Ruhan Swar <73247272+Broccoli201@users.noreply.github.com>
2022-03-27 13:01:22 -0700

    asd
    asd

--- links.py
+++ links.py
@@ -8,7 +8,9 @@ https: // www.pinterest.com/pin/160088961739053448/  # background
 
 https://opengameart.org/content/animated-top-down-zombie  # link to invaders
 
+https://opengameart.org/content/animated-top-down-survivor-player    #link to main character
 
-
-
+https://github.com/nealholt/python_programming_curricula/blob/master/CS1/0550_galaga/pygame_galaga2_shoot_any_direction.py   # help from here
+ 
+ 
 HAVE TO LINK THE MAIN CHARACTER
