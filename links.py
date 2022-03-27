cc13a6e33d41aab34f6daef79c8c806e1fa9937b
Ruhan Swar <73247272+Broccoli201@users.noreply.github.com>
2022-03-26 18:33:37 -0700

    charcter moves and bullets
    asda

--- links.py
+++ links.py
@@ -5,4 +5,10 @@ http: // decrodedart.blogspot.com/    # final boss
 
 https: // www.pinterest.com/pin/160088961739053448/  # background
 
+
+https://opengameart.org/content/animated-top-down-zombie  # link to invaders
+
+
+
+
 HAVE TO LINK THE MAIN CHARACTER
--- main.py
+++ main.py
@@ -1,24 +1,207 @@
 import pygame
 import os
+import random
+
 pygame.font.init()
 
+
+pygame.init()
+
 # initilizing the SCREENdow screen
-WIDTH, HEIGHT = 1000, 900
+WIDTH, HEIGHT = 800, 1000
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-BACKGROUND = (234, 212, 252)
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("background.jpg")), (WIDTH, HEIGHT))
+MAINCHARACTER = pygame.transform.scale(pygame.image.load(os.path.join("MAINCHARACTER.png")), (55, 50))
+BULLET = pygame.transform.scale(pygame.image.load(os.path.join("bullet.png")), (20, 32))
+INVADER1 = pygame.transform.scale(pygame.image.load(os.path.join("invader.png")), (55, 50))
+INVADER2 = pygame.transform.scale(pygame.image.load(os.path.join("invader.png")), (75, 60))
+INVADER3 = pygame.transform.scale(pygame.image.load(os.path.join("invader.png")), (85, 70))
+
+MC_imge = pygame.transform.rotate(MAINCHARACTER, 90)
+Bullet_image = pygame.transform.rotate(BULLET,-90)
+Invader1_image = pygame.transform.rotate(INVADER1,90)
+Invader2_image = pygame.transform.rotate(INVADER2,90)
+Invader3_image = pygame.transform.rotate(INVADER3,90)
 
 
 # ADD NAME
-pygame.display.set_caption(
-    " !!!@@@@ ----  ENTER NAME LATER  ----@@@@!!!  - Prithvi Rao")
+pygame.display.set_caption( " !!!@@@@ ----  ENTER NAME LATER  ----@@@@!!!  - Prithvi Rao")
+
+#PARENT CLASSES
+class Bullet:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = Bullet_image
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return not(self.y <= height and self.y >= 0)
+
+    def collision(self, obj):
+        return collide(self, obj)
+
+
+
+
+class Character:            # parent class for the defenders and invaders
+
+    COOLDOWN = 60
+
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.MC_img = MC_imge             #pass later in the inheritence 
+        self.INVADER_img = None
+        self.bullet_img = Bullet_image        
+        self.bullets = []                         #LIST FOR bulletS
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.MC_img, (self.x, self.y))
+        for bullet in self.bullets:
+            bullet.draw(window)
+
+    def move_bullets(self, vel):
+        self.cooldown()
+        for bullet in self.bullets:
+            bullet.move(vel)
+            if bullet.off_screen(HEIGHT):
+                self.bullets.remove(bullet)
+
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            bullet = Bullet(self.x, self.y, self.bullet_img)
+            self.bullets.append(bullet)
+            self.cool_down_counter = 1
+
+    def get_width(self):
+        return self.MC_img.get_width()
+
+    def get_height(self):
+        return self.MC_img.get_height()
+
+# child classes
+
+class MainCharacter(Character):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.MC_img = MAINCHARACTER
+        self.bullet_img = BULLET
+        self.mask = pygame.mask.from_surface(self.MC_img)
+        self.max_health = health
+
+    def move_bullets(self, vel):
+        self.cooldown()
+        for bullet in self.bullets:
+            bullet.move(vel)
+            if bullet.off_screen(HEIGHT):
+                self.bullets.remove(bullet)
+
+
+    def draw(self, window):
+        super().draw(window)
+        self.healthbar(window)
+
+    def healthbar(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.MC_img.get_height() + 10, self.MC_img.get_width(), 10))
+        pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.MC_img.get_height() + 10, self.MC_img.get_width() * (self.health/self.max_health), 10))
+
+
+
+
+
+
+
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
+
 
 
 # initializing the characters and the images
 run = True
+FPS = 65
+bullet_vel = 25
+level = 0
+lives = 1
+player = Character(900, 900)
+enemies = []
+player_vel = 1
+main_font = pygame.font.SysFont("comicsans", 50)
+lost_font = pygame.font.SysFont("comicsans", 70)
+wave_length = 5
+lost = False
+clock = pygame.time.Clock() 
+
+
+def redraw_win():
+    SCREEN.blit(BACKGROUND, (0,0))
+
+    lives_show = main_font.render(f"lives: {lives}", 1, (0,0,255))
+    level_show = main_font.render(f"level: {level}", 1, (255,0,0))
+
+    SCREEN.blit(lives_show, (10, 10))
+    SCREEN.blit(level_show, (10, 100))
+
+    player.draw(SCREEN)
+    
+
+    
+    pygame.display.update()
+
+
+
+
 
 while run:
+    clock.tick(FPS)
+    redraw_win()
+
+    if len(enemies) == 0:
+        level += 1
+        wave_length += 5
+    
+
+    if lives <= 0 or player.health <= 0:
+        lost = True
+
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-main_menu()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and player.y - player_vel > 0:                                                             #up
+        player.y -= player_vel
+
+    if keys[pygame.K_a] and player.x -player_vel > 0 :                                                             #left
+        player.x -= player_vel
+
+    if keys[pygame.K_d] and player.x +player_vel + player.get_width() < WIDTH:                                     #right
+        player.x += player_vel
+
+    if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT:                                  #down
+        player.y += player_vel
+
+    if keys[pygame.K_SPACE]:
+        player.shoot()
+
+
+    player.move_bullets(-bullet_vel)
