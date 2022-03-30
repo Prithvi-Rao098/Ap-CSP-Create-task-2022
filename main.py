e1622877cebc85e7a12e57fb270d7ce056cc98cf
Prithvi Rao <73247272+Broccoli201@users.noreply.github.com>
2022-03-30 13:38:47 -0700

    asd
    asd

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ class Enemy(Character):
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
     offset_y = obj2.y - obj1.y
-    return float(obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None)
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
 
 # initializing the characters and the images
